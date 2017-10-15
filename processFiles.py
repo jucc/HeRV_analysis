3580bb0638b7068e9339540f7d74e637f1d836ec
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2017-10-15 03:57:22 -0200

    improving function names and documentation

--- processFiles.py
+++ processFiles.py
@@ -6,28 +6,17 @@ import re
 import csv
 
 
+"""
+lists all filenames in current directory matching the given regexp
+"""
 def getFilesMatching(regexp):
     return list(filter(lambda x: re.match(regexp, x), os.listdir()))
 
 
-def getBeatsFromFile(filename):
-    try:
-        lines = list(filter(lambda x : len(x) > 3, open(filename).readlines()))
-        return list(map(lambda x: int(x.split(',')[-1].strip()), lines))
-    except:
-        return None
-
-
-def getBeatsByHour(year, month, day, hour):    
-    regexp = r"^rr%s%s%s%s" % (year, month, day, hour)
-    match = getFilesMatching(regexp)
-    if match:
-        return getBeatsFromFile(match[0])
-    else:
-        return None
-
-
-def getFileContents(filename):
+"""
+lists all intervals (datetime and interval length) in a file
+"""
+def getIntervalsFromFile(filename):
     lines = list(filter(lambda x : len(x) > 3, open(filename, 'r').readlines()))
     reader = csv.reader(lines, delimiter=',')
     rows = []
@@ -35,10 +24,33 @@ def getFileContents(filename):
         if len(row) > 1:
             rows.append({'date': row[0], 'interval':int(row[1])})
     return rows
+
+
+"""
+lists the lengths of all intervals contained in a file
+"""
+def getRawIntervalsFromFile(filename):
+    try:
+        lines = list(filter(lambda x : len(x) > 3, open(filename).readlines()))
+        return list(map(lambda x: int(x.split(',')[-1].strip()), lines))
+    except:
+        return None
+
+
+"""
+lists all intervals (datetime and interval length) recorded in a specific hour
+@input: year, month, day and hour as two-digit strings
+Example: getIntervalsByHour('17', '10', '01', '14')
+"""
+def getIntervalsByHour(year, month, day, hour):    
+    regexp = r"^rr%s%s%s%s" % (year, month, day, hour)
+    match = getFilesMatching(regexp)
+    if match:
+        return getIntervalsFromFile(match[0])
+    else:
+        return None
     
 
 RAW_DATA_PATH = "C:\\Users\\julia\\Google Drive\\Academics\\Mestrado\\HRV\\RawData\\0"
-filename = 'rr17100115.csv'
 os.chdir(RAW_DATA_PATH)
-#print (getBeatsByHour('17', '10', '01', '15'))
-print (getFileContents(filename))
+print (getIntervalsByHour('17', '10', '01', '14'))

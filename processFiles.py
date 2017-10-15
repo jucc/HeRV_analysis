646ab88b850bb97f095aea1ad962964dbe6ad6af
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2017-10-15 18:48:30 -0200

    cleaning up the functions a bit

--- processFiles.py
+++ processFiles.py
@@ -15,6 +15,20 @@ def getFileMatching(regexp):
 
 
 """
+returns the name of the file that contains intervals for the hour of a datetime
+If the file does not exits for this hour, will return None
+"""
+def getCorrespondingFilename(dt):
+    filepattern = datetime.strftime(dt, '%y%m%d%H')
+    regexp = r"^rr%s" % (filepattern)
+    match = getFileMatching(regexp)
+    if match:
+        return match[0]
+    else:
+        return None
+
+
+"""
 lists all intervals (datetime and interval length) in a file
 """
 def getIntervalsFromFile(filename):
@@ -27,45 +41,35 @@ def getIntervalsFromFile(filename):
     return rows
 
 
-"""
-lists the lengths of all intervals contained in a file
-"""
-def getRawIntervalsFromFile(filename):
-    try:
-        lines = list(filter(lambda x : len(x) > 3, open(filename).readlines()))
-        return list(map(lambda x: int(x.split(',')[-1].strip()), lines))
-    except:
-        return None
-
 
 """
-lists all intervals (datetime and interval length) recorded in a specific hour
-@param[in]: dt - datetime with hour precision, will disregard minutes and seconds
+lists all intervals in a given hour
+@param[in] dt - datetime with the hour queried. Minutes and seconds will be disregarded
 Example: 
+dt = datetime.strptime('2017-10-01 14:15:16', "%Y-%m-%d %H:%M:%S")
+getIntervalsByHour(dt)
+will return all intervals recorded for 2017-10-01 from 14:00:00 to 14:59:59
 """
 def getIntervalsByHour(dt):
-    filepattern = datetime.strftime(dt, '%y%m%d%H')
-    regexp = r"^rr%s" % (filepattern)
-    match = getFileMatching(regexp)
-    if match:
-        return getIntervalsFromFile(match[0])
+    filename = getCorrespondingFilename(dt)
+    if filename:
+        return getIntervalsFromFile(filename)
     else:
-        return None
+        return []
     
-    
+   
 """
 lists all intervals (datetime and interval length) recorded in a period of time
 between start_dt and end_dt
 @param[in]: start_dt and end_dt as datetimes
 """
 def getIntervals(start_dt, end_dt):
+    
     return []
 
     
 
 RAW_DATA_PATH = "C:\\Users\\julia\\Google Drive\\Academics\\Mestrado\\HRV\\RawData\\0"
 os.chdir(RAW_DATA_PATH)
-td = '2017-10-01 14:15:16'
-bi = datetime.strptime(td, "%Y-%m-%d %H:%M:%S")
-print(bi.hour)
-print (getIntervalsByHour(bi))
+dt = datetime.strptime('2017-10-01 14:15:16', "%Y-%m-%d %H:%M:%S")
+print (getIntervalsByHour(dt))

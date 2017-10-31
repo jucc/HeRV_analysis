7b3e701bb93be67eb3724be4e3df6e50911f0507
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2017-10-31 06:30:25 -0200

    extracting session with start and stop instead of two separate events

--- convertActivities.py
+++ convertActivities.py
@@ -26,43 +26,60 @@ def extractActivitiesFromFile(filename):
     lines = list(filter(lambda x : len(x) > 1, open(filename, 'r').readlines()))
     reader = csv.reader(lines, delimiter=',')
     rows = []
+    sess = {}
     for row in reader:
-        if isStartRow:
-            act = addStart(row)
-            rows.append(act)
-        elif isStopRow:
-            act = addStop(row)
-            rows.append(act)
+        
+        if isStartRow(row):
+            # if there's an unfinished session pending, flush it first
+            if sess.get('start') != None: 
+                rows.append(sess)
+            sess = startSession(row)
+            
+        elif isStopRow(row):
+            # if there is no started session, ignore this stop command
+            if sess.get('start') != None: 
+                act = stopSession(row, sess)
+                rows.append(act)
+                sess = {}
+            
         else:
             print ("Unidentified activity type in: %s"%row)
+            
     return rows
 
 
 def parseActivities(dirname):
     os.chdir(dirname)
+    sessions = []
     for file in getActivityFilenames:
-        extractActivitiesFromFile(file)
+        sessions.extend(extractActivitiesFromFile(file))
+
 
 def isStartRow(row):
-    return len(row) > 3 and row[1] == 'start'
+    return (len(row) > 3 and row[1] == 'start')
 
 def isStopRow(row):
-    return len(row) > 1 and row[1] == 'stop'
+    return (len(row) > 1 and row[1] == 'stop')
 
 def timeFromString(timestr):
     return datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S")
 
-def addStart(row):
-    return {'start': timeFromString(row[0]),
+def startSession(row):
+    sess = {'start': timeFromString(row[0]),
             'activity': row[2], 
             'posture': row[3] }
+    if len(row) > 4:
+        sess['notes'] = row[4]
+    return sess
     
-def addStop(row):
-    return {'stop': timeFromString(row[0])}
+def stopSession(row, sess):
+    sess['stop'] = timeFromString(row[0])
+    return sess
+
 
 if __name__ == '__main__':
     RAW_DATA_PATH = "C:\\Users\\julia\\Google Drive\\Academics\\Mestrado\\HeRV\\RawData\\0"
     os.chdir(RAW_DATA_PATH)
-    #parseActivities()
+    #print(parseActivities(RAW_DATA_PATH))
     filename = 'act171013.csv'
     print (extractActivitiesFromFile(filename))

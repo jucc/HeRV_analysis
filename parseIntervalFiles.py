09a2d7c6e9f4b38e54c30b497b089693bcd4a719
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2017-11-03 18:43:20 -0200

    parse intervals also using input dir as parameter instead oh doing chdir

--- parseIntervalFiles.py
+++ parseIntervalFiles.py
@@ -15,10 +15,10 @@ returns the name of the file that contains intervals for a datetime
 Minutes and seconds are disregarded, the file represents the whole hour in the datetime
 If no file corresponding to this hour exists, will return None
 """
-def getFilename(dt):
+def getFilename(dt, dirname='.'):
     filepattern = datetime.strftime(dt, '%y%m%d%H')
     regexp = r"^rr%s" % (filepattern)
-    f = list(csvu.getFilenames(dirname = '.', regexp=regexp))
+    f = list(csvu.getFilenames(dirname = dirname, regexp=regexp))
     if len(f) > 0:
         return f[0]
     else:
@@ -46,8 +46,8 @@ dt = datetime.strptime('2017-10-01 14:15:16', "%Y-%m-%d %H:%M:%S")
 getIntervalsByHour(dt)
 will return all intervals recorded for 2017-10-01 from 14:00:00 to 14:59:59
 """
-def getIntervalsByHour(dt):
-    filename = getFilename(dt)
+def getIntervalsByHour(dt, dirname="."):
+    filename = getFilename(dt, dirname)
     if filename:
         return getIntervalsFromFile(filename)
     else:
@@ -59,21 +59,20 @@ lists all intervals (datetime and interval length) recorded in a period of time
 between start_dt and end_dt
 @param[in]: start_dt and end_dt as datetimes
 """
-def getIntervals(start_dt, end_dt):
+def getIntervals(start_dt, end_dt, dirname="."):
     
     intervals =[]
     for i in range(int((end_dt-start_dt).seconds/3600)):
-        intervals.extend(getIntervalsByHour(start_dt + timedelta(hours=i)))    
+        intervals.extend(getIntervalsByHour(start_dt + timedelta(hours=i), dirname))
     return list(filter(lambda x: x['date'] > start_dt and x['date'] < end_dt, intervals))
 
     
 if __name__ == '__main__':
     RAW_DATA_PATH = "C:\\Users\\julia\\Google Drive\\Academics\\Mestrado\\HeRV\\RawData\\0"
-    os.chdir(RAW_DATA_PATH)
     dt = datetime.strptime('2017-10-01 14:15:16', "%Y-%m-%d %H:%M:%S")
-    mfilename=getFilename(dt)
+    mfilename=getFilename(dt, RAW_DATA_PATH)
     print(mfilename)
     print('#intervals: %d'%len(getIntervalsFromFile(mfilename)))
-    print('#intervals in 1 hour : %d'%len(getIntervalsByHour(dt)))
-    print('#intervals in 3 hours: %d'%len(getIntervals(dt, dt+timedelta(hours=3))))
+    print('#intervals in 1 hour : %d'%len(getIntervalsByHour(dt, RAW_DATA_PATH)))
+    print('#intervals in 3 hours: %d'%len(getIntervals(dt, dt+timedelta(hours=3), RAW_DATA_PATH)))
     

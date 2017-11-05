2b9f16c2b00e6df751109953d75baecd7324fca5
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2017-11-05 19:49:39 -0200

    consolidating and printing

--- consolidateFiles.py
+++ consolidateFiles.py
@@ -10,34 +10,65 @@ import parseActivityFiles as paf
 #pun intended :)
 from datetime import datetime
 
-MIN_SESS = 300
-
 """
 for each session read from activity files, adds the corresponding list of RR 
 intervals contained in the time frame between its start and stop
 """
 def getAllDataBySession(dirname, verbose=True):
+    
     sessions = paf.parseActivityFiles(dirname, verbose)
     
     for sess in sessions:        
         sess['duration'] = int((sess['stop']-sess['start']).seconds)        
         sess['rr'] = pif.getIntervals(sess['start'], sess['stop'], dirname)        
         if verbose:
-            print (sess['start'], sess['stop'])
-            minutes = sess['duration']/60
-            rrcount = len(sess['rr'])
-            print("duration: %d min, rr: %d, avg bpm: %d"%(minutes, rrcount, rrcount/minutes))
+            printSession(sess)
             
-    valid_sess = list(filter(lambda x: x['duration'] > MIN_SESS and len(x['rr']) > MIN_SESS, sessions))
-    print ("%d valid sessions out of %d sessions registered"
-           %(len(valid_sess), len(sessions)))
+    return sessions
+
+
+def getValidSessions(sessions, min_len):
+    return list(filter(lambda x: x['duration'] >= min_len and len(x['rr']) >= min_len, sessions))
+
+
+def getInvalidSessions(sessions, min_len):
+    return list(filter(lambda x: x['duration'] < min_len or len(x['rr']) < min_len, sessions))
                 
+
+def printSession(sess, forsheet=False, user=''):
+    minutes = rrcount = 0
+    if sess.get('duration'):
+        minutes = sess['duration']/60
+    if sess.get('rr'):
+        rrcount = len(sess['rr'])    
+    basicinfo = (minutes, rrcount)
+    if forsheet:
+        print("%s, %s, %s, "%(user,sess['activity'],sess['posture'])
+              + str(sess['start']) + ", " + str(sess['stop'])
+              + ", %d, %d"%basicinfo )
+    else:
+        print(str(sess['start']) + "\t%d min\t%d beats"%basicinfo)
+        
+
+def printSpreadsheetHeader():
+    return " user, activity, posture, start, stop, duration, intervals"
+
 
 def timeFromString(timestr):
     return datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S")
 
+
 if __name__ == '__main__':
     
     RAW_DATA_PATH = "C:\\Users\\julia\\Google Drive\\Academics\\Mestrado\\HeRV\\RawData\\0"
-    data = getAllDataBySession(RAW_DATA_PATH)
+    data = getAllDataBySession(RAW_DATA_PATH, verbose=False)
+    valid = getValidSessions(data, 300)
+    invalid = getInvalidSessions(data, 300)
+    print ("%d valid sessions out of of %d registered"%(len(valid), len(data)))
+    print ("making sure: %d invalid sessions", len(invalid))
+    #for sess in valid:
+    #    printSession(sess, forsheet=True, user='0')
+    for sess in invalid:
+        printSession(sess, forsheet=False)
+    
         

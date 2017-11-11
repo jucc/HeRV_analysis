5031226444c35078c0fb798b04f7cc92e52e3cd6
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2017-11-11 12:00:16 -0200

    using scikit report

--- hervpd.py
+++ hervpd.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.preprocessing import scale
 from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV, train_test_split
-
+from sklearn.metrics import classification_report
 
 
 def filterActivities(df, includelist):
@@ -41,47 +41,61 @@ def scaleWithinUser(df):
         dfs.append(u)    
     return pd.concat(dfs)
     
+    
+def report(testdf, result, labelName, verbose=False):
+    ytrue = [x for x in testdf[labelName]]
+    print(classification_report(ytrue, result))
+        
 
-def printResults(expected, result, verbose=False):
-    if verbose: 
-        print("expected\t\tresult")
-        print("------------------------------")
-    correct = 0
-    for i in range(len(expected)):
-        if verbose:
-            print ("%s\t\t%s"%(expected[i], result[i]))
-        if expected[i] == result[i]:
-            correct += 1    
-    print("%d out of %d right! :)"%(correct,len(expected)))
+def runSVM(train, test, labelName, kernelName, grid_params, crossval):
+    grid = GridSearchCV(svm.SVC(kernel=kernelName, cache_size=1000), param_grid=grid_params, cv=crossval)
+    grid.fit(X=train.iloc[:, 3:15], y=train[labelName])
+    print("Best params for %s kernel: %s with score %0.5f"%(kernelName, grid.best_params_, grid.best_score_))
+    clf = svm.SVC(kernel=kernelName, cache_size=1000, C=grid.best_params_['C']) # hoe to send params in here?
+    clf.fit(X=train.iloc[:, 3:15], y=train[labelName])
+    print ('--- test results for linear kernel:')
+    report(test, clf.predict(test.iloc[:, 3:15]), labelName)
 
-
-def runFlow(df, labelName='activity'):    
+def runFlow(df, labelName='activity'):
+    
+    # preprocess dataset
+    
     df = scaleWithinUser(df)
     train, test = train_test_split(df, test_size=0.2)
-    print("%d train examples and %d test examples"%(len(train),len(test)))
+    print("%d train examples and %d test examples"%(len(train),len(test)))        
+    
+    # define cross- validation and grid search parameters
+    
     crossval = StratifiedShuffleSplit(n_splits=4, test_size=0.2)
     c_range = np.logspace(-1, 2, 4) 
-    gamma_range = np.logspace(-2, 1, 4)
+    gamma_range = np.logspace(-2, 1, 4)    
+    
+    # linear kernel
+    
     param_lin=dict(C=c_range)
-    param_rbf=dict(C=c_range, gamma=gamma_range)
     grid_lin = GridSearchCV(svm.SVC(kernel='linear', cache_size=1000), param_grid=param_lin, cv=crossval)
     grid_lin.fit(X=train.iloc[:, 3:15], y=train[labelName])
-    print("Best params for linear kernel: %s with score %0.5f" % (grid_lin.best_params_, grid_lin.best_score_))
-    grid_rbf = GridSearchCV(svm.SVC(kernel='rbf', cache_size=1000), param_grid=param_rbf, cv=crossval)
-    grid_rbf.fit(X=train.iloc[:, 3:15], y=train[labelName])
-    print("Best params for RBF kernel: %s with score %0.5f" % (grid_rbf.best_params_, grid_rbf.best_score_))
+    print("Best params for linear kernel: %s with score %0.5f" % (grid_lin.best_params_, grid_lin.best_score_))    
     clf1 = svm.SVC(kernel='linear', cache_size=1000, C=grid_lin.best_params_['C'])
     clf1.fit(X=train.iloc[:, 3:15], y=train[labelName])
     print ('--- test results for linear kernel:')
-    printResults(test[labelName].values, clf1.predict(test.iloc[:, 3:15]))
+    report(test, clf1.predict(test.iloc[:, 3:15]), labelName)
+        
+    # rbf kernel
+    
+    param_rbf=dict(C=c_range, gamma=gamma_range)
+    grid_rbf = GridSearchCV(svm.SVC(kernel='rbf', cache_size=1000), param_grid=param_rbf, cv=crossval)
+    grid_rbf.fit(X=train.iloc[:, 3:15], y=train[labelName])
+    print("Best params for RBF kernel: %s with score %0.5f" % (grid_rbf.best_params_, grid_rbf.best_score_))
     clf2 = svm.SVC(kernel='rbf', cache_size=1000, C=grid_rbf.best_params_['C'], gamma=grid_rbf.best_params_['gamma'])    
     clf2.fit(X=train.iloc[:, 3:15], y=train[labelName])
     print ('--- test results for RBF kernel:')
-    printResults(test[labelName].values, clf1.predict(test.iloc[:, 3:15]), verbose=False)
+    report(test, clf2.predict(test.iloc[:, 3:15]), labelName)
 
     
-    
-    
-
-    
+def runFlowByUser(df, labelName='activity'):
+    for user in df.user.unique():
+        print ("CLASSFIER FOR USER %s"%user)
+        runFlow(userRows(df, user), labelName)
+        
     

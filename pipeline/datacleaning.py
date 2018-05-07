72b7c7da27286fda1aed9deb405bdd95dd26c9a6
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2018-05-07 02:10:58 +0000

    adding features and dataviz for sessions

--- pipeline/datacleaning.py
+++ pipeline/datacleaning.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
 
-def clean_rr_series(rr):
+def clean_rr_series(rr, min=300, max=1800):
     """
     basic filter to remove clearly invalid interval values 
     intervals must be in the range 300 <= rr <= 1800
     """
-    return [{'date': x['date'], 'interval': x['interval']} for x in rr if x['interval'] < 1800 and x['interval'] > 300]
+    return [{'date': x['date'], 'interval': x['interval']} for x in rr if x['interval'] < max and x['interval'] > min]
